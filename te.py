13ac6297dfb9879b2f4257a912d0aea4ba0c59b4
[L]ED <raiderfan237@protonmail.com>
2023-05-17 11:39:43 -0600

    Added some code

--- te.py
+++ te.py
@@ -1,10 +1,15 @@
+import pyautogui,time
 
+time.sleep(5) #Sleeps for 5 seconds before the program starts
 
-
-
-
-
-
+distance = 200
+while distance > 0:
+        pyautogui.drag(distance, 0, duration=0.5)   # move right
+        distance -= 10
+        pyautogui.drag(0, distance, duration=0.5)   # move down
+        pyautogui.drag(-distance, 0, duration=0.5)  # move left
+        distance -= 5
+        pyautogui.drag(0, -distance, duration=0.5)  # move up
 
 
 
