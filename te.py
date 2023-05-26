3ff1db70c10fc01906b1135d213989b99ec4b574
[L]ED <110116836+yummyhomie@users.noreply.github.com>
2023-05-26 12:34:31 -0600

    Added countdown and got pyautogui to work

--- te.py
+++ te.py
@@ -1,17 +1,25 @@
 import pyautogui
 import time
 
-time.sleep(5)
+import time
 
-pyautogui.write('Hello!')
-pyautogui.press('space')
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(f"Program will start in {i} seconds...")
+        time.sleep(1)
 
-time.sleep(10)
+countdown_duration = 6 # Set the countdown duration in seconds
+countdown(countdown_duration) # Display the message and countdown
 
-pyautogui.write('PREPARE TO DIE!')
+pyautogui.write('Student called in and was needing help logging into their myUVU portal. FERPA verified them and updated their verification methods. They were then able to reset their password and log in!')
+pyautogui.press('shift')
 pyautogui.press('enter')
 
+time.sleep(2)
+
 quit()
+
+
 
 
 
