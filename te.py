30f7b7304697962ad477d814a95e7da066451da5
[L]ED <110116836+yummyhomie@users.noreply.github.com>
2023-05-30 11:37:54 -0600

    Attempted to add options to choose from upon starting the program :)

--- te.py
+++ te.py
@@ -1,13 +1,35 @@
 import pyautogui
 import time
 
-import time
+#def countdown(seconds):
+#    for i in range(seconds, 0, -1):
+#        print(f"Program will start in {i} seconds...")
+#        time.sleep(1)
 
-def countdown(seconds):
-    for i in range(seconds, 0, -1):
-        print(f"Program will start in {i} seconds...")
-        time.sleep(1)
+#countdown_duration = 6 # Set the countdown duration in seconds
+#countdown(countdown_duration) # Display the message and countdown
 
+def dp_menu():
+    print("Select from the following you freaking liberal.")
+    print("[1]")
+    print("[2]")
+    print("[ESC]")
+
+def main():
+     dp_menu()
+     choice = input()
+
+     if choice == '1': option1()
+     elif choice == '2': option2()
+     elif choice == 'esc': print("o7") 
+
+else: print('Bruh. Try again. ')
+
+def option1():
+        def countdown(seconds):
+        for i in range(seconds, 0, -1):
+            print(f"Program will start in {i} seconds...")
+            time.sleep(1)
 countdown_duration = 6 # Set the countdown duration in seconds
 countdown(countdown_duration) # Display the message and countdown
 
@@ -15,7 +37,18 @@ pyautogui.write('Student called in and was needing help logging into their myUVU
 pyautogui.press('shift')
 pyautogui.press('enter')
 
-time.sleep(2)
+#                                                 :)                                           
+
+def option2():
+    def countdown(second):
+    for i in range(seconds, 0, -1):
+         print(f"Program will start in {i} seconds...")
+         time.sleep(1)
+countdown_duration = 6
+countdown(countdown_duration)
+
+pyautogui.write('Student was needing help logging into their myUVU portal. FERPA verified them and set up their authentication methods. I was then able to show them how to reset their password. They were then able to log in!')
+
 
 quit()
 
