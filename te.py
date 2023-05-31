76e1b8c08d7a63a1e2d5f0b30af6692f07f8e4b0
[L]ED <110116836+yummyhomie@users.noreply.github.com>
2023-05-31 11:28:09 -0600

    Added test.py, updated te.py

--- te.py
+++ te.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import msvcrt
 
 #def countdown(seconds):
 #    for i in range(seconds, 0, -1):
@@ -9,54 +10,44 @@ import time
 #countdown_duration = 6 # Set the countdown duration in seconds
 #countdown(countdown_duration) # Display the message and countdown
 
-def dp_menu():
-    print("Select from the following you freaking liberal.")
-    print("[1]")
-    print("[2]")
-    print("[ESC]")
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(f"Program will start in {i} seconds...")
+        time.sleep(1)
 
-def main():
-     dp_menu()
-     choice = input()
-
-     if choice == '1': option1()
-     elif choice == '2': option2()
-     elif choice == 'esc': print("o7") 
-
-else: print('Bruh. Try again. ')
-
-def option1():
-        def countdown(seconds):
-        for i in range(seconds, 0, -1):
-            print(f"Program will start in {i} seconds...")
-            time.sleep(1)
-countdown_duration = 6 # Set the countdown duration in seconds
-countdown(countdown_duration) # Display the message and countdown
+def pg1():
+    countdown_duration = 10 # Set the countdown duration in seconds
+    countdown(countdown_duration) # Display the message and countdown
 
 pyautogui.write('Student called in and was needing help logging into their myUVU portal. FERPA verified them and updated their verification methods. They were then able to reset their password and log in!')
-pyautogui.press('shift')
-pyautogui.press('enter')
 
-#                                                 :)                                           
-
-def option2():
-    def countdown(second):
-    for i in range(seconds, 0, -1):
-         print(f"Program will start in {i} seconds...")
-         time.sleep(1)
-countdown_duration = 6
-countdown(countdown_duration)
+def pg2():
+    countdown_duration = 10
+    countdown(countdown_duration)
 
 pyautogui.write('Student was needing help logging into their myUVU portal. FERPA verified them and set up their authentication methods. I was then able to show them how to reset their password. They were then able to log in!')
 
+def pg3():
+    quit()
 
-quit()
+def dp_menu():
+    print("Select one of the following:")
+    print("[1]")
+    print("[2]")
+    print("[z] to exit")
 
+while True:
+    dp_menu()
 
+    choice = msvcrt.getch().decode("utf-8")
 
-
-
-
+    if choice == "1":
+       pg1()
+    elif choice == "2":
+      pg2()
+    elif choice == "z":
+      pg3()
+    else: print('Bruh. Try again. ')
 
 
 
